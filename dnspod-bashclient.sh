1488305c7c1d4b324d99def66f41b818c2bffc1c
Weihong Guan <weihong.guan@gmail.com>
2013-04-02 23:04:09 +0800

    redirect submit response to /dev/null

--- dnspod-bashclient.sh
+++ dnspod-bashclient.sh
@@ -4,7 +4,7 @@ source info.sh
 
 value=$(curl -s ifconfig.me)
 echo $value
-curl -sk "https://dnsapi.cn/Record.Modify" -d "$account&$record&value=$value"
+curl -sk "https://dnsapi.cn/Record.Modify" -d "$account&$record&value=$value" > /dev/null
 
 if [ $? -eq 0 ]; then
 	echo "ip: $value of $sub_domain of Domain(id: $domain_id) submitted successfully."
