6b3802c9c3bf60c56b6b2e250e192388ba6cefc5
Weihong Guan <weihong.guan@gmail.com>
2014-05-26 17:19:02 +0800

    fix fetch_records line slice

--- fetch_domains.sh
+++ fetch_domains.sh
@@ -6,4 +6,3 @@ echo "domains info:"
 
 curl -sk "https://dnsapi.cn/Domain.List" -d "$account" \
 	| egrep "<(id|name)" | egrep -o ">[^<]+<" | tr -d "<>" | xargs -n 2 echo
-
--- fetch_records.sh
+++ fetch_records.sh
@@ -6,7 +6,8 @@ echo "records info:"
 curl -sk "https://dnsapi.cn/Record.List" -d "$account&domain_id=$domain_id" > /tmp/record.xml.$$
 
 line=$(cat /tmp/record.xml.$$ | grep -n "<records>" | egrep -o "^[^:]+")
-tail -n +$line /tmp/record.xml.$$ | egrep -o ">[^<]+<" | tr -d "<>" | grep -v "hold" | xargs -n 10 echo \
+
+tail -n +$line /tmp/record.xml.$$ | egrep -o ">[^<]+<" | tr -d "<>" | grep -v "hold" | xargs -n 11 echo \
 	| awk '{print $1 "\t" $2 "\t" $4 "\t" $6 "\t" $7 "\t" $8 "\t" $9 $10 "\t" $5}' 
 
 rm -f /tmp/record.xml.$$
